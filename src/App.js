6daa1387f0c0acae6f84a7f87b33cbc126a788a3
Pratt Vaidya <pratt.vaidya@ttt.studio>
2021-11-16 19:43:35 -0800

    Add capability for offline videos

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import './App.css';
+import video from './videos/sculpture.mp4'
 
 function App() {
-  const [data, setData] = useState([])
+  // const [data, setData] = useState([])
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await fetch("https://orangevalleycaa.org/api/videos")
-      .then(response => response.json())
-      setData(result)
-    }
-    fetchData()
-  }, [])
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const result = await fetch("https://orangevalleycaa.org/api/videos")
+  //     .then(response => response.json())
+  //     setData(result)
+  //   }
+  //   fetchData()
+  // }, [])
 
   return (
     <div className="App">
       <header>
         <h1>Videos</h1>
       </header>
-      {data.map(video => (
+      {/* {data.map(video => (
         <div key={video.id}>
-          <h2>{video.name}</h2>
-          <video height={200} controls src={video.video_url} />
-        </div>
-      ))}
+          <h2>{video.name}</h2> */}
+          <video height={200} controls src={video} />
+        {/* </div> */}
+      {/* ))} */}
     </div>
   );
 }
